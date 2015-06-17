5a1e1388c365fd1543d214f1f8cfd58138360b75
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-06-17 00:41:42 -0400

    Refactoring and Commenting.

---
+++ TicTacPortal/start.rb
@@ -0,0 +1,11 @@
+require_relative 'game'
+
+
+# This game is a tribute to Valve's Portal Series.
+#   All rights are reserved this publisher
+#   I will not make any money from this.
+#   Support their release. 
+
+# Starts new game session.
+player = Session::Player.new	
+player.start
